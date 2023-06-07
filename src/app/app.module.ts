0fb64ce730859cf521a94dcfeffffc195aa87bdd
thiccnugget <samuelecammaraa@gmail.com>
2023-06-07 16:56:02 +0200

    aggiunti componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: 'listing', component: ProductsComponent },
+  { path: 'detail/:id', component: DetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    HomeComponent,
+    FavoritesComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
