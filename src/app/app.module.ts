dd196006fe625773a76f59ca9981bbf08d683df0
thiccnugget <samuelecammaraa@gmail.com>
2023-06-07 17:05:59 +0200

    zio pera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { DetailsComponent } from './details/details.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomeComponent,
+    ProductsComponent,
+    DetailsComponent,
+    FavoritesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
