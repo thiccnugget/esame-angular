c98a4a95c14aeae287cf34a224d239ceed620b90
thiccnugget <samuelecammaraa@gmail.com>
2023-06-08 23:10:49 +0200

    (revisionare) aggiunto footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
   { path: '404', component: NotFoundComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'details/:id', component: DetailsComponent },
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
