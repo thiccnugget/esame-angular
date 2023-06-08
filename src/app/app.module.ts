c9f94f3568dd8fbdbbc93a81e4623bfe9ea22dc8
thiccnugget <samuelecammaraa@gmail.com>
2023-06-08 16:27:28 +0200

    fix app module + fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductsComponent } from './products/products.component';
-import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { DetailsComponent } from './details/details.component';
-import { HomeComponent } from './home/home.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '404', component: NotFoundComponent },
-  { path: 'listing', component: ProductsComponent },
-  { path: 'detail/:id', component: DetailsComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/404' }
-];
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
-    ProductsComponent,
-    LoginComponent,
-    NotFoundComponent,
-    DetailsComponent,
-    HomeComponent,
-    FavoritesComponent,
-    SignUpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  
 
 }
