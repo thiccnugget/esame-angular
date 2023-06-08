86d0cd00ef8536674a91408165d991f9e4dc6dee
frasc93 <scollofrancesca93@gmail.com>
2023-06-08 22:57:17 +0200

    add products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: any[] = [];
 
+  ngOnInit() {
+    axios.get('https://dummyjson.com/products')
+      .then(response => {
+        const responseData = response.data;
+        if (Array.isArray(responseData)) {
+          this.products = responseData;
+        } else if (typeof responseData === 'object' && responseData.hasOwnProperty('products')) {
+          this.products = responseData.products;
+        } else {
+          console.error('Dati API non validi:', responseData);
+        }
+      })
+      .catch(error => {
+        console.error('Errore durante la chiamata API:', error);
+      });
+  }
 }
