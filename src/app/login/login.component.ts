8f179c7c9471e5ddf32d63966e554ab181828eb8
robertociffo <roccociffo2.0@gmail.com>
2023-06-11 17:46:33 +0200

    modifica del controllo password e username

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalStorageService } from '../utils/localStorage/local-storage.service';
 import { Router } from '@angular/router';
+import * as bcrypt from 'bcryptjs';
 
 @Component({
   selector: 'app-login',
@@ -44,7 +45,7 @@ export class LoginComponent implements OnInit {
     if (this.loginFormGroup.valid) {
       const { username, password } = this.loginFormGroup.value;
       const storedUser = this.localStorage.getUserData();
-      if (storedUser && username === storedUser.username && password === storedUser.password) {
+      if (storedUser && username === storedUser.username && bcrypt.compareSync(password, storedUser.password)) {
         this.isLoggedIn = true;
         this.router.navigate(['/home']);
         console.log('Login successful');
@@ -57,7 +58,7 @@ export class LoginComponent implements OnInit {
   }
 
   signup() : void {
-    this.router.navigate(['/signup']);
+    this.router.navigate(['signup']);
   }
   logout(): void {
     this.isLoggedIn = false;
