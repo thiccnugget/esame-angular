03de1608b6c92938321426135791c9a293c4522e
robertociffo <roccociffo2.0@gmail.com>
2023-06-11 16:48:21 +0200

    add sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,38 +1,35 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import ValidatorsCustom from '../utils/customValidators';
 import { LocalStorageService } from '../utils/localStorage/local-storage.service';
+import { Router } from '@angular/router';
 
-const dataUpdated = {
-  email: undefined,
-  username: undefined
-}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   isLoggedIn = false;
   currentUsername: string | null = null;
+  loginFormGroup!: FormGroup;
+
   constructor(
     private fb: FormBuilder,
-    private localStorage: LocalStorageService
-    ) {}
-
-  submitted = false;
-
-  exampleForm!: FormGroup;
-
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const storedUser = this.localStorage.getUserData();
-    this.isLoggedIn = storedUser !== null;
-    if(storedUser){
+    if (storedUser) {
+      this.isLoggedIn = true;
       this.currentUsername = storedUser.username;
     }
-    
 
+    this.loginFormGroup = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
     /*if (storedUser) {
       console.log('Dati presenti nel localStorage:', storedUser);
       alert('Dati presenti nel localStorage: ' + JSON.stringify(storedUser));
@@ -40,40 +37,30 @@ export class LoginComponent implements OnInit{
       console.log('Non ci sono dati nel localStorage.');
       alert('Non ci sono dati nel localStorage.');
     }*/
-
-    this.exampleForm = this.fb.group({
-      username: [storedUser?.username || '', Validators.required],
-      email: [storedUser?.email || '', Validators.compose([
-        Validators.required, Validators.minLength(12)
-      ])],
-      password: ['password', ValidatorsCustom.createPasswordStrengthValidator()]
-    });
   }
+  
 
-  onSubmit = () => {
-    this.submitted = true;
-    if (this.exampleForm.valid) {
-      const {username, email} = this.exampleForm.value;
-      this.localStorage.saveUserData(username, email, '');
-      this.isLoggedIn = true;
-      console.log('User data saved into localStorage');
-      
+  onSubmit() {
+    if (this.loginFormGroup.valid) {
+      const { username, password } = this.loginFormGroup.value;
+      const storedUser = this.localStorage.getUserData();
+      if (storedUser && username === storedUser.username && password === storedUser.password) {
+        this.isLoggedIn = true;
+        this.router.navigate(['/home']);
+        console.log('Login successful');
+      } else {
+        console.log('Invalid username or password');
+      }
     } else {
       console.log('Form not valid');
     }
   }
 
-  logout(): void{
+  signup() : void {
+    this.router.navigate(['/signup']);
+  }
+  logout(): void {
     this.isLoggedIn = false;
   }
-  /*ngOnDestroy() {
-    this.clearUserData();
-  }
-
-  clearUserData(): void {
-    this.localStorage.clearUserData();
-    this.isLoggedIn = false;
-    console.log('User data removed from localStorage');
-  }*/
 }
 
