bebe14d8197f238a88c3bf8def506e7aa984cd3d
robertociffo <roccociffo2.0@gmail.com>
2023-06-11 17:08:37 +0200

    sign-up and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
@@ -11,10 +9,13 @@ import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
-import { FavoritesComponent } from './favorites/favorites.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { CartComponent } from './cart/cart.component';
+//import { FooterComponent } from "./footer/footer.component";
+//import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -22,31 +23,32 @@ const routes: Routes = [
   { path: 'listing', component: ProductsComponent },
   { path: 'detail/:id', component: DetailsComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'favorites', component: FavoritesComponent },
+  //{ path: 'cart', component: CartComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    
-    ProductsComponent,
-    LoginComponent,
-    NotFoundComponent,
-    DetailsComponent,
-    HomeComponent,
-    FavoritesComponent,
-    SignUpComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        NotFoundComponent,
+        DetailsComponent,
+        HomeComponent,
+        SignUpComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        //FooterComponent,
+        //MatToolbarModule,
+        //NavbarComponent
+    ]
 })
 export class AppModule { }
+
