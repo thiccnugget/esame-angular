139821459fc256c5800523d637952c82915bf1dd
LucaH404 <luca.sapienza03@gmail.com>
2023-06-12 12:35:27 +0200

    made content responsive

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart/cart.service';
+import axios from 'axios';
 
-interface CartItem {
-    id: number;
-    name: string;
-    price: number;
-  }
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  
-  cartItems: CartItem[] = [];
+  products:any;
+  cartItems: any
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartItems = this.cartService.getCartItems();
+    console.log('this is my product', this.cartItems[0].product);
+    }
 }
+//  const products: any[] = [];
+  
+//     this.cartItems.forEach(item => {
+//       axios.get(`https://dummyjson.com/products/${item.id}`)
+//         .then(response => {
+//           const product = response.data;
+//           console.log(response.data)
+//           products.push(product);
+//         })
+//         .catch(error => {
+//           console.error(error);
+//         });
+//     });
+  
+//     this.products = products; // Assign the products array to the component property
+//     console.log('Products:', products);
+//   }
