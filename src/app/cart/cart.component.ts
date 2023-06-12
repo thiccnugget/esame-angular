080cae9aaa335c1bcbe1e6bd768d0c9b87a32d61
thiccnugget <samuelecammaraa@gmail.com>
2023-06-12 11:33:54 +0200

    feat: Cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Cart, Product, ProductDetails} from '../utils/types';
+import { LocalStorageService } from '../utils/localStorage/local-storage.service';
+import axios from 'axios';
+import { RouterModule } from '@angular/router';
 
-interface CartItem {
-    id: number;
-    name: string;
-    price: number;
-  }
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  standalone: true,
+  imports:[CommonModule, RouterModule]
 })
+
+
 export class CartComponent {
-  
-  cartItems: CartItem[] = [];
+  localStorage = new LocalStorageService();
+  products: ProductDetails[] = [];
+
+  ngOnInit() {
+    const elements = this.localStorage.getCart()
+    elements.map((item:Cart)=>{
+      axios.get("https://dummyjson.com/products/"+item.id)
+      .then((response) => this.products.push({...response.data, quantity: item.qty}))
+      .catch((error) => console.log(error))
+    })
+    console.log(this.products)
+  }
 }
